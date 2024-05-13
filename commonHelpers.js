76f1db7cb5f546073bb94fc75bc7e4582a9bec84
Alona93Hryhorieva <151534732+Alona93Hryhorieva@users.noreply.github.com>
2024-05-13 10:40:16 +0000

    Deploying to gh-pages from @ Alona93Hryhorieva/goit-js-hw-11@dd7d914f939d9af4175bb7a6edc74d9aee8442c1 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,25 +1,25 @@
 import{i as l,S as d}from"./assets/vendor-8c59ed88.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))o(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const a of t.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&o(a)}).observe(document,{childList:!0,subtree:!0});function r(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function o(e){if(e.ep)return;e.ep=!0;const t=r(e);fetch(e.href,t)}})();const f="43820023-fa202629be5215ad836dbfc98",p="https://pixabay.com/api/",m=(i="cat")=>{const s=new URLSearchParams({key:f,q:i,image_type:"photo",orientation:"horizontal",safesearch:!0});return fetch(`${p}?${s}`).then(r=>{if(!r.ok)throw new Error(r.statusText);return r.json()})},y=i=>i.map(({webformatURL:s,largeImageURL:r,tags:o,likes:e,view:t,comments:a,downloads:u})=>`<li class="gallery-item">
-                       <a class="gallery-link js-gallery-link" href='${r}'>
-                         <img src='${s}' alt='${o}' title='${o}'>
+                       <a class="gallery-link js-gallery-link" href="${r}">
+                         <img src="${s}" alt="${o}" title="${o}">
                       </a>
 
                       <div class="image-details">
                          <ul class="details-title">
                              <li class="details-title-det">
                                   <h3 class="title-info">Likes:</h3>
-                                  <p class="paragraph-info">'${e}'</p>
+                                  <p class="paragraph-info">${e}</p>
                                 </li>
                                 <li class="details-title-det">
                                  <h3 class="title-info">Views:</h3>
-                                 <p class="paragraph-info">'${t}'</p>
+                                 <p class="paragraph-info">${t}</p>
                                </li>  
                              <li class="details-title-det">
                                  <h3 class="title-info">Comments:</h3>
-                                 <p class="paragraph-info">'${a}'</p>
+                                 <p class="paragraph-info">${a}</p>
                              </li>  
                               <li class="details-title-det">
                                   <h3 class="title-info">Downloads:</h3>
-                                  <p class="paragraph-info">'${u}'</p>
+                                  <p class="paragraph-info">${u}</p>
                               </li>
                  
                             </ul>
