efdbfee418192589dd160080e07390071629c746
Alona93Hryhorieva <151534732+Alona93Hryhorieva@users.noreply.github.com>
2024-05-13 13:28:45 +0300

    13/28

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,32 +1,37 @@
 export const createGalleryItemMarkup = images => {
-  return images
-    .map(
-        ({ webformatURL, largeImageURL, tags, likes, view, comments, downloads }) => 
-        `<li class="gallery-item">
-         <a class="gallery-link js-gallery-link" href='${image.largeImageURL}'>
-              <img src='${image.webformatURL}' alt='${image.tags}' title='${image.tags}'>
-         </a>
-         <div class="image-details">
-              <ul class="details-title">
-                 <li class="details-title-det">
-                     <h3 class="title-info">Likes:</h3>
-                     <p class="paragraph-info">'${image.likes}'</p>
-                  </li>
-                 <li class="details-title-det">
-                     <h3 class="title-info">Views:</h3>
-                     <p class="paragraph-info">'${image.views}'</p>
-                 </li>  
-                 <li class="details-title-det">
-                     <h3 class="title-info">Comments:</h3>
-                      <p class="paragraph-info">'${image.comments}'</p>
-                  </li>  
-                  <li class="details-title-det">
-                      <h3 class="title-info">Downloads:</h3>
-                      <p class="paragraph-info">'${image.downloads}'</p>
-                  </li>
-             </ul>
-         </div>
-      </li>`
-    ).join('');
+    return images
+        .map(
+            ({ webformatURL, largeImageURL, tags, likes, view, comments, downloads }) => {
+                return (
+                   `<li class="gallery-item">
+                       <a class="gallery-link js-gallery-link" href='${largeImageURL}'>
+                         <img src='${webformatURL}' alt='${tags}' title='${tags}'>
+                      </a>
+
+                      <div class="image-details">
+                         <ul class="details-title">
+                             <li class="details-title-det">
+                                  <h3 class="title-info">Likes:</h3>
+                                  <p class="paragraph-info">'${likes}'</p>
+                                </li>
+                                <li class="details-title-det">
+                                 <h3 class="title-info">Views:</h3>
+                                 <p class="paragraph-info">'${views}'</p>
+                               </li>  
+                             <li class="details-title-det">
+                                 <h3 class="title-info">Comments:</h3>
+                                 <p class="paragraph-info">'${comments}'</p>
+                             </li>  
+                              <li class="details-title-det">
+                                  <h3 class="title-info">Downloads:</h3>
+                                  <p class="paragraph-info">'${downloads}'</p>
+                              </li>
+                 
+                            </ul>
+                      </div>
+                  </li>`
+                );
+            };
+       ).join('');
 }
       
